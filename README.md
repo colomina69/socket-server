b43375fe2299c5b96f7822d6a2a6f32e249dc8eb
Antonio Joaquin Colomina Mira <benicolo@gmail.com>
2021-07-18 08:16:58 +0200

    Primer commit - rest serer

---
+++ README.md
@@ -0,0 +1,17 @@
+
+
+# Socket-Server
+
+
+Reconstruir modulos de Node
+'''
+npm install
+
+Generar el Dist
+
+tsc -w
+
+levantar servidor
+
+nodemon dist/
+node dist/
---
+++ index.ts
@@ -0,0 +1,24 @@
+import Server from './clases/server';
+import { SERVER_PORT } from './global/enviroment';
+import router from './routes/router';
+import bodyParser from 'body-parser'
+import express from 'express';
+import cors from 'cors';
+const server=new Server();
+
+
+
+server.app.use(express.urlencoded({extended:true}));
+server.app.use(express.json());
+
+//cors
+server.app.use(cors({origin:true,credentials:true}))
+
+server.app.use('/',router)
+
+server.start(()=>{
+console.log(`Servidor corriento en puerto ${SERVER_PORT}` );
+    
+})
+
+
